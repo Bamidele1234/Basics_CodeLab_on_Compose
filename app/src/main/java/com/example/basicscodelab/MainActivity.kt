e7ab839bf8fca86c49c83e5e2edb79a77f2a7d1e
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-03-10 19:56:16 +0100

    Improved the UI and added an Onboarding Screen

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,18 +3,21 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodeLabTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +31,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")){
+fun MyApp(names: List<String> = listOf("World", "Compose", "Bamidele")){
     Surface(color = MaterialTheme.colors.background) {
         Column(modifier = Modifier.padding(vertical = 4.dp)) {
             for(name in names) {
@@ -41,19 +44,60 @@ fun MyApp(names: List<String> = listOf("World", "Compose")){
 
 
 @Composable
+fun OnboardingScreen(){
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface {
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { shouldShowOnboarding = false}
+            ){
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodeLabTheme {
+        OnboardingScreen()
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false)}
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
         ) {
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp)){
-
-            Text(text = "Hello,")
-            Text(text = name)
-            
+        Row(modifier = Modifier.padding(24.dp)){
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding))
+            {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value}
+            ) {
+                Text(if (expanded.value) "Show less" else "show more")
+            }
         }
+
     }
 }
 
