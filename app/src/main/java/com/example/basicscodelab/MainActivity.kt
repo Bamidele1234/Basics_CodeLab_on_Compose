5541f4cdde375531bfbc202533ccd4bf1a843b85
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-03-12 13:51:36 +0100

    Improved the UI and added an Onboarding Screen

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodeLabTheme
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 
 class MainActivity : ComponentActivity() {
@@ -31,22 +36,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose", "Bamidele")){
-    Surface(color = MaterialTheme.colors.background) {
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            for(name in names) {
-                Greeting(name = name)
-            }
-        }
+fun MyApp(){
+    // TODO: This state should be hoisted and check out what a property delegate is
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+    } else {
+        Greetings()
     }
 }
 
+@Composable
+fun Greetings(names: List<String> = List(1000) { "$it" }){
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
 
 @Composable
-fun OnboardingScreen(){
-    // TODO: This state should be hoisted
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+fun OnboardingScreen(onContinueClicked: () -> Unit){
 
     Surface {
         Column (
@@ -57,7 +68,7 @@ fun OnboardingScreen(){
             Text("Welcome to the Basics Codelab!")
             Button(
                 modifier = Modifier.padding(vertical = 24.dp),
-                onClick = { shouldShowOnboarding = false}
+                onClick = onContinueClicked
             ){
                 Text("Continue")
             }
@@ -69,42 +80,61 @@ fun OnboardingScreen(){
 @Composable
 fun OnboardingPreview() {
     BasicsCodeLabTheme {
-        OnboardingScreen()
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
 @Composable
 fun Greeting(name: String) {
 
-    val expanded = remember { mutableStateOf(false)}
+    var expanded by rememberSaveable { mutableStateOf(false)}
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-        ) {
+    ) {
         Row(modifier = Modifier.padding(24.dp)){
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding))
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp)))
             {
                 Text(text = "Hello,")
                 Text(text = name)
             }
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value}
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "show more")
+                Text(if (expanded) stringResource(R.string.show_less) else stringResource(R.string.show_more))
             }
         }
 
     }
 }
 
+@Composable
+private fun CardContent(name: String) {
+    // TODO: Add more contents here
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodeLabTheme {
-       MyApp()
+       Greetings()
     }
 }
