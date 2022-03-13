30866a4f5abdd8bd216ed0800aa5f23d38cf27b5
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-03-13 17:22:52 +0100

    Animated the list, Updated the README.md and added the finishing touches

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Basics CodeLab on Compose
 An introduction to the basics of Jetpack Compose
 
-# What I learnt 
+## What I learnt 
+- Reusing composables
 - State and State Hoisting in Compose
 - Compose Theming
 - Creating a performant lazy list
 - Enabling previews in both Dark and Light mode
 - Fundamentals of declarative UI
 - Basic UI elements such as Row and Column
+- State persistence
 
 ## Note
 **This was done in completion of activity 4: Jetpack Compose Basics**
--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,13 +4,16 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodeLabTheme
@@ -76,6 +80,13 @@ fun OnboardingScreen(onContinueClicked: () -> Unit){
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "OnBoardingPreview Dark"
+)
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
@@ -86,43 +97,61 @@ fun OnboardingPreview() {
 
 @Composable
 fun Greeting(name: String) {
-
-    var expanded by rememberSaveable { mutableStateOf(false)}
-
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)){
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp)))
-            {
-                Text(text = "Hello,")
-                Text(text = name)
-            }
-            OutlinedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text(if (expanded) stringResource(R.string.show_less) else stringResource(R.string.show_more))
-            }
-        }
-
+    ){
+        CardContent(name)
     }
 }
 
 @Composable
 private fun CardContent(name: String) {
-    // TODO: Add more contents here
+    var expanded by rememberSaveable { mutableStateOf(false)}
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("This text repeats itself try to read through it" +
+                            "The lazy dog jumps over the brown fox nearly 10 times").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
 }
+
 
 @Preview(
     showBackground = true,
